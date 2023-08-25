97a6abe24cd5f3632f8108d6874267097cc803dc
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-25 16:54:36 +0200

    added page comics index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main');
+
+@section('title', 'Comics List')
+
+@section('main-content')
+    <main>
+        <ul>
+            @forelse ($comics as $comic)
+                <li>
+                    <h4>{{ $comic['title'] }}</h4>
+                </li>
+            @empty
+                <h3 class="text-danger">
+                    Nessun fumetto disponibile
+                </h3>
+            @endforelse
+        </ul>
+        <a href="{{ route('home') }}">Torna alla home</a>
+    </main>
+@endsection
