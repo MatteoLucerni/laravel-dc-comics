af8bd02c1a256e84d9dbd4a67f7bcd4920ca8c93
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-25 16:54:27 +0200

    added page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main');
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@section('title', 'Home')
 
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <div class="container">
-        <h1>TEST</h1>
-        <ul>
-            @forelse ($comics as $comic)
-                <li>
-                    <h4>{{ $comic['title'] }}</h4>
-                </li>
-            @empty
-            @endforelse
-        </ul>
-    </div>
-</body>
-
-</html>
+@section('main-content')
+    <a href="{{ route('comics.index') }}">Accedi</a>
+@endsection
