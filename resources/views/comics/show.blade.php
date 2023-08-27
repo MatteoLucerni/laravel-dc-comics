33a384b59992a1269a37a190a19434e9cc912a1a
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-27 15:15:53 +0200

    added structure

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,6 +4,14 @@
 
 @section('main-content')
     <main>
-        <h1>{{ $comic['title'] }}</h1>
+        <img src="{{ $comic->thumb }}" alt="">
+        <h1>{{ $comic->title }}</h1>
+        <p>{{ $comic->series }}</p>
+        <h3>{{ $comic->price }}</h3>
+        <p>Tipo: {{ $comic->type }}</p>
+        <p>Data: {{ $comic->sale_date }}</p>
+        <p>Artisti: {{ $comic->artists }}</p>
+        <p class="mb-5">Scrittori: {{ $comic->writers }}</p>
+        <a href="{{ route('comics.index') }}">Torna alla lista fumetti</a>
     </main>
 @endsection
