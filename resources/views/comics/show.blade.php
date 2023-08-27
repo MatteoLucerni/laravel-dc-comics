177a28eea82d5f78f24c4b695efd37cd5d26dde4
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-27 15:18:24 +0200

    styled image and infos

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,14 +4,16 @@
 
 @section('main-content')
     <main>
-        <img src="{{ $comic->thumb }}" alt="">
-        <h1>{{ $comic->title }}</h1>
-        <p>{{ $comic->series }}</p>
-        <h3>{{ $comic->price }}</h3>
-        <p>Tipo: {{ $comic->type }}</p>
-        <p>Data: {{ $comic->sale_date }}</p>
-        <p>Artisti: {{ $comic->artists }}</p>
-        <p class="mb-5">Scrittori: {{ $comic->writers }}</p>
-        <a href="{{ route('comics.index') }}">Torna alla lista fumetti</a>
+        <img class="mb-5" style="max-height: 500px" src="{{ $comic->thumb }}" alt="">
+        <div class="border border-info rounded p-5">
+            <h1>{{ $comic->title }}</h1>
+            <p>{{ $comic->series }}</p>
+            <h3>{{ $comic->price }}</h3>
+            <p>Tipo: {{ $comic->type }}</p>
+            <p>Data: {{ $comic->sale_date }}</p>
+            <p>Artisti: {{ $comic->artists }}</p>
+            <p class="mb-5">Scrittori: {{ $comic->writers }}</p>
+            <a href="{{ route('comics.index') }}">Torna alla lista fumetti</a>
+        </div>
     </main>
 @endsection
