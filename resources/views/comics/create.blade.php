a29ba59c9fe739ebb46d9ab557c74f5ca5342190
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-28 10:25:42 +0200

    added form directives

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,49 +6,50 @@
     <main>
         <div class="card-header">
             <h1>Aggiungi il tuo fumetto</h1>
-            <a href="{{ route('teams.index') }}">Indietro</a>
+            <a href="{{ route('comics.index') }}">Indietro</a>
         </div>
         <div class="card-body">
-            <form action="">
+            <form action="{{ route('comics.store') }}" method="POST">
+                @csrf
                 <div class="row">
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="title" class="form-label">Titolo</label>
-                            <input required type="text" class="form-control" id="title">
+                            <input name="title" required type="text" class="form-control" id="title">
                         </div>
                         <div class="mb-3">
                             <label for="thumb" class="form-label">Immagine (link)</label>
-                            <input required type="url" class="form-control" id="thumb">
+                            <input name="thumb" required type="url" class="form-control" id="thumb">
                         </div>
                         <div class="mb-3">
                             <label for="price" class="form-label">Prezzo (in $)</label>
-                            <input required type="number" class="form-control" id="price">
+                            <input name="price" required type="number" class="form-control" id="price">
                         </div>
                         <div class="mb-3">
-                            <label for="serie" class="form-label">Serie</label>
-                            <input required type="text" class="form-control" id="serie">
+                            <label for="series" class="form-label">Serie</label>
+                            <input name="series" required type="text" class="form-control" id="series">
                         </div>
                         <div class="mb-3">
                             <label for="sale_date" class="form-label">Data</label>
-                            <input required type="text" class="form-control" id="sale_date">
+                            <input name="sale_date" required type="date" class="form-control" id="sale_date">
                         </div>
                         <div class="mb-3">
                             <label for="type" class="form-label">Tipo</label>
-                            <input required type="text" class="form-control" id="type">
+                            <input name="type" required type="text" class="form-control" id="type">
                         </div>
                         <div class="mb-3">
                             <label for="artists" class="form-label">Artisti</label>
-                            <input required type="text" class="form-control" id="artists">
+                            <input name="artists" required type="text" class="form-control" id="artists">
                         </div>
                         <div class="mb-3">
                             <label for="writers" class="form-label">Scrittori</label>
-                            <input required type="text" class="form-control" id="writers">
+                            <input name="writers" required type="text" class="form-control" id="writers">
                         </div>
                     </div>
                     <div class="col-12">
-                        <div class="mb-3 form-floating">
-                            <textarea class="form-control" placeholder="Descrizione..." id="floatingTextarea2" style="height: 100px"></textarea>
-                            <label for="floatingTextarea2">Descrizione</label>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Descrizione</label>
+                            <input name="description" required type="text" class="form-control" id="description">
                         </div>
                     </div>
                 </div>
