3e0c4c488cbb4c35a076304b138f5fce9b251958
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-28 11:00:36 +0200

    added list of links

---
+++ config/links.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    [
+        'text' => 'Characters',
+        'url' => '/',
+        'current' => false,
+        'route' => 'characters'
+    ],
+    [
+        'text' => 'Comics',
+        'url' => '/',
+        'current' => true,
+        'route' => '/'
+    ],
+    [
+        'text' => 'Movies',
+        'url' => '/',
+        'current' => false,
+        'route' => 'movies'
+    ],
+    [
+        'text' => 'TV',
+        'url' => '/',
+        'current' => false,
+        'route' => 'tv'
+    ],
+    [
+        'text' => 'Games',
+        'url' => '/',
+        'current' => false,
+        'route' => 'games'
+    ],
+    [
+        'text' => 'Collectibles',
+        'url' => '/',
+        'current' => false,
+        'route' => 'collectibles'
+    ],
+    [
+        'text' => 'Videos',
+        'url' => '/',
+        'current' => false,
+        'route' => 'videos'
+    ],
+    [
+        'text' => 'Fans',
+        'url' => '/',
+        'current' => false,
+        'route' => 'fans'
+    ],
+    [
+        'text' => 'News',
+        'url' => '/',
+        'current' => false,
+        'route' => 'news'
+    ],
+    [
+        'text' => 'Shop',
+        'url' => '/',
+        'current' => false,
+        'route' => 'shop'
+    ],
+];
